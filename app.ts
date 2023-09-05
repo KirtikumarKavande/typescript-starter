376303ab3e3097f3128430dee36c3f57120b149f
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-09-05 11:52:19 +0530

    cofiguration typscript

--- app.ts
+++ app.ts
@@ -2,13 +2,15 @@ const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
 const buttonElement = document.querySelector("button");
 
-function add(a: number, b: number) {
+function add(a:any, b: number) {
   return a + b;
 }
 
-buttonElement?.addEventListener("click", () => {
-  const num1 = num1Element.value;
-  const num2 = num1Element.value;
-  var ans = add(+num1, +num2);
-  console.log(ans);
-});
+if (buttonElement) {
+  buttonElement.addEventListener("click", () => {
+    const num1 = num1Element.value;
+    const num2 = num1Element.value;
+    var ans = add(+num1, +num2);
+    console.log(ans);
+  });
+}
