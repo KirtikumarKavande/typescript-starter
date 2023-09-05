f6a6f3e21dc6c92f5e243095a921b361ddf2ed3f
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-09-05 15:00:28 +0530

    union in ts

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+var num1Element = document.getElementById("num1");
+var num2Element = document.getElementById("num2");
+var buttonElement = document.querySelector("button");
 function add(a, b) {
-  return a + b;
+    return a + b;
 }
-var ans = add("10", "11");
-console.log(ans);
+buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener("click", function () {
+    var num1 = num1Element.value;
+    var num2 = num1Element.value;
+    var ans = add(+num1, +num2);
+    console.log(ans);
+});
--- app.ts
+++ app.ts
@@ -1,16 +1,18 @@
 const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
-const buttonElement = document.querySelector("button");
+const buttonElement = document.querySelector("button")!;
 
-function add(a:any, b: number) {
-  return a + b;
+function add(a: number | string, b: number | string) {
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  } else if (typeof a === "string" && typeof b === "string") {
+    return a + " " + b;
+  }
 }
 
-if (buttonElement) {
-  buttonElement.addEventListener("click", () => {
-    const num1 = num1Element.value;
-    const num2 = num1Element.value;
-    var ans = add(+num1, +num2);
-    console.log(ans);
-  });
-}
+buttonElement.addEventListener("click", () => {
+  const num1 = num1Element.value;
+  const num2 = num1Element.value;
+  var ans = add(num1, num2);
+  console.log(ans);
+});
