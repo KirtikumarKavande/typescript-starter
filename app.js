4541b11b6b019e9c374ad5a5309239ecb2637a46
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-09-05 10:10:03 +0530

    starting off ts

---
+++ app.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+  return a + b;
+}
+var ans = add("10", "11");
+console.log(ans);
---
+++ app.ts
@@ -0,0 +1,5 @@
+function add(a: number, b: number) {
+  return a + b;
+}
+var ans = add("10", "11");
+console.log(ans);
