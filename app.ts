42fd4b0b08c1688d2dcab82e50badaf1e92e3c90
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-09-05 10:29:56 +0530

    type interference and type casting

--- app.ts
+++ app.ts
@@ -1,5 +1,14 @@
+const num1Element = document.getElementById("num1") as HTMLInputElement;
+const num2Element = document.getElementById("num2") as HTMLInputElement;
+const buttonElement = document.querySelector("button");
+
 function add(a: number, b: number) {
   return a + b;
 }
-var ans = add("10", "11");
-console.log(ans);
+
+buttonElement?.addEventListener("click", () => {
+  const num1 = num1Element.value;
+  const num2 = num1Element.value;
+  var ans = add(+num1, +num2);
+  console.log(ans);
+});
